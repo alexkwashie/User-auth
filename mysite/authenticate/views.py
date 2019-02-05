b78f84380455df9d9ee12946d2619b174af41269
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-05 20:25:03 +0000

    adding login pages

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login , logout
 
 # Create your views here.
 def home(request):
     return render (request, 'authenticate_home/home.html', {})
+
+def login_user(request):
+    return render (request, 'authenticate_home/login.html', {})
