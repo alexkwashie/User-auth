f25d44105acf81340f0c314d150536cef49f07ea
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-05 21:45:06 +0000

    adding logout link

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login , logout
+from django.contrib  import messages
 
 # Create your views here.
 def home(request):
@@ -12,10 +13,12 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request,('Yay!, You are logged in'))
             return redirect('home')
         else:
+            messages.success(request,('Login Error'))
             return redirect('login')
-            
+
     else:
         return render (request, 'authenticate_home/login.html', {})
 
