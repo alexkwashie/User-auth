4a2affdd546f4284085399315aa847c828394938
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-02-08 20:11:06 +0000

    adding Profile page

--- mysite/authenticate/forms.py
+++ mysite/authenticate/forms.py
@@ -4,9 +4,9 @@ from django import forms
 
 #Use Widgets on django doc to add styling to the from
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField( widget=forms.TextInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length =100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label= "Enter Email",widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(label= "Enter First Name",max_length = 100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label= "Enter Last Name",max_length =100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
@@ -16,5 +16,13 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm,self).__init__(*args,**kwargs)
 
         self.fields['username'].widget.attrs['class']='form-control'
+        self.fields['username'].label= 'Username'
+        self.fields['username'].help_text='<span><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
         self.fields['password1'].widget.attrs['class']='form-control'
+        self.fields['password1'].label= 'Password'
+        self.fields['password1'].help_text=' '
+
         self.fields['password2'].widget.attrs['class']='form-control'
+        self.fields['password2'].label= 'Confirm Password'
+        self.fields['password2'].help_text=' '
